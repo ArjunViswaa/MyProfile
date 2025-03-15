123691108376bfc1e2682e4090eb096414da701c
Arjun Viswaa <arjunviswaa@gmail.com>
2025-03-15 07:08:58 +0530

    Work in progress

--- portfolio/src/components/Dashboard.jsx
+++ portfolio/src/components/Dashboard.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div id="loadingScreen"
+        className="fixed w-full h-screen z-1000 bg-transparent backdrop-blur-sm  justify-center items-center top-0 flex flex-col"
+    >
+      <img src="assets/images/work-in-progress.gif" className='w-[400px] h-[200px]'/>
+      <h2 className='my-14'>Thanks for visiting... Site will be up soon !!!</h2>
+    </div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
