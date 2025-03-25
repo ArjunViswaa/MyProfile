7e2b1c52c264af5e638de1e9fe42198dc7bb6007
Arjun Viswaa <arjunviswaa@gmail.com>
2025-03-25 22:00:54 +0530

    Minor changes

--- portfolio/src/components/Contacts.jsx
+++ portfolio/src/components/Contacts.jsx
@@ -4,7 +4,7 @@ function Contacts() {
   return (
     <div id="contact" className='flex flex-col gap-4 order-3 md:order-3'>
         <div className="bg-[#18181b] text-white pt-6 p-3 rounded-lg flex flex-col items-center h-[45vh]">
-          <h2 className="text-2xl font-bold">Connect</h2>
+          <h2 className="text-xl font-bold">Connect</h2>
           <div className='flex flex-col mt-5 md:w-[100%] w-[50%] h-[70%]'>
             <div className='flex flex-row h-[30%]'>
               <div id="gmail" className='h-[100%] w-[45%] bg-white rounded-xl justify-items-center content-center cursor-pointer transition-transform duration-500 hover:!scale-110'>
--- portfolio/src/components/ProfileAndEducation.jsx
+++ portfolio/src/components/ProfileAndEducation.jsx
@@ -4,7 +4,7 @@ function ProfileAndEducation() {
     return (
         <div id="projects-and-Education" className='flex flex-col gap-4 order-2 md:order-1'>
             <div className="bg-[#18181b] text-white p-6 rounded-lg flex flex-col items-center h-[45vh] cursor-pointer transition-transform duration-500 hover:!scale-105">
-                <h2 className="text-2xl font-bold">Project Studio</h2>
+                <h2 className="text-xl font-bold">Project Studio</h2>
                 <div className="w-40 h-40 flex items-center justify-center rounded-full border-25 border-green-400 mt-4">
                     <span className="text-2xl text-blue-400">100%</span>
                 </div>
@@ -12,9 +12,9 @@ function ProfileAndEducation() {
                 <p className="text-gray-400">Completed 10+ Projects</p>
             </div>
             <div className="bg-[#18181b] text-white p-6 rounded-lg flex flex-col items-center mt-5 h-[35vh] cursor-pointer transition-transform duration-500 hover:!scale-105">
-                <h2 className="text-2xl font-bold">Education</h2>
+                <h2 className="text-xl font-bold">Education</h2>
                 <img src='/assets/images/education.png' className='h-40 w-40' />
-                <h2>MS in Computer Science</h2>
+                <h3 style={{fontSize: "0.8rem"}}>MS in Computer Science</h3>
                 <p className='text-xs'>( 2024 to pursuing )</p>
             </div>
         </div>
